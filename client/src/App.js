620313e0df801cf68f649b6f16cdf8ad361d21ab
Anthony Hill <hillan1152@gmail.com>
2019-10-31 19:22:51 -0400

    Completed Dashboard and need to add tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Dashboard from './components/Dashboard';
 
 import './App.css';
 
@@ -6,49 +7,53 @@ function App() {
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
 
-  const addStrike = (strike) => {
-    if(strike >=2){
-      return 0
-    } else {
-      return strike + 1;
-    }
-  }
+  // const addStrike = (strike) => {
+  //   if(strike >=2){
+  //     return 0
+  //   } else {
+  //     return strike + 1;
+  //   }
+  // }
   
-  const addBall = (ball) => {
-    if(ball >=3){
-      return 0
-    } else {
-      return ball + 1;
-    }
-  }
+  // const addBall = (ball) => {
+  //   if(ball >=3){
+  //     return 0
+  //   } else {
+  //     return ball + 1;
+  //   }
+  // }
 
-  const restart = (restart) => {
-    setBalls(0);
-    setStrikes(0);
-  }
+  // const restart = (restart) => {
+  //   setBalls(0);
+  //   setStrikes(0);
+  // }
   
-  const foulBall = (strike) => {
-    if(strike < 2) {
-      return strike + 1
-    } else {
-      return strike
-    }
-  }
+  // const foulBall = (strike) => {
+  //   if(strike < 2) {
+  //     return strike + 1
+  //   } else {
+  //     return strike
+  //   }
+  // }
 
-  const hit = () => {
-    restart() 
-  };
+  // const hit = () => {
+  //   restart() 
+  // };
   return (
     <div className="App">
-      <div className="balls">Balls: {balls}</div>
-      <div className="strikes">Strikes: {strikes}</div>
-
-      <button onClick={() => setStrikes(addStrike)}>STRIKE!</button>
-      <button onClick={() => setBalls(addBall)}>BALL!</button>
-      <button onClick={() => setStrikes(foulBall)}>Foul!</button>
-      <button onClick={() => hit(restart)}>Hit!</button>
+        <div className="balls">Balls: {balls}</div>
+        <div className="strikes">Strikes: {strikes}</div>
+        <Dashboard balls={balls} strikes={strikes} setBalls={setBalls} setStrikes={setStrikes}/>
     </div>
   );
 }
 
 export default App;
+
+{/* <div className="balls">Balls: {balls}</div>
+<div className="strikes">Strikes: {strikes}</div>
+
+<button onClick={() => setStrikes(addStrike)}>STRIKE!</button>
+<button onClick={() => setBalls(addBall)}>BALL!</button>
+<button onClick={() => setStrikes(foulBall)}>Foul!</button>
+<button onClick={() => hit(restart)}>Hit!</button> */}
